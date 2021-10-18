bab3c40eee08d97cff92f645cd6f432d4912f226
Hannah Brog <bugsbrog@Hannahs-MBP.home>
2021-10-18 14:33:47 -0600

    tried to add pic

---
+++ african-marketplace/src/components/HomePage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+<div className='image'>
+    <img src = 'https://images.unsplash.com/photo-1488459716781-31db52582fe9?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=3600' alt='grocery-store' />
+
+</div>
