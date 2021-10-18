2831b266ab162746906c1a3f57e902421e4fe993
BillyBernard <billybernard54@gmail.com>
2021-10-18 14:42:31 -0600

    working on app/index/nav

--- african-marketplace/src/components/NavBar.js
+++ african-marketplace/src/components/NavBar.js
@@ -8,7 +8,14 @@ const NavBar = () => {
 
 return (
     <div className="nav-header">
-
+        <header>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/login">Login</Link>
+                <Link to="/logout">Logout</Link>
+                <Link to="/signup">Sign Up</Link>
+            </nav>
+        </header>
     </div>
     );
 };
