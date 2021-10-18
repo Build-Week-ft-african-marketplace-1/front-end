4455d526c7971489a41609146c0b7fee76c307d9
BillyBernard <billybernard54@gmail.com>
2021-10-18 13:23:38 -0600

    more page setup

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -1,10 +1,17 @@
 
+import { Route, Link, Switch } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { useHistory } from 'react-router';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>test</h1>
+      
+
+
     </div>
   );
 }
