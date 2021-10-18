592fc9ba8e18457f2fb208a9cebc1a3ae610a94e
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-10-18 17:45:28 -0400

    login progress

--- african-marketplace/src/components/Login.js
+++ african-marketplace/src/components/Login.js
@@ -1,30 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 // import yup
+
+const initialUserValues = {
+  username: "",
+  password: "",
+};
+
+const initialFormErrors = {
+  username: "",
+  password: "",
+};
 
 export default function Login(props) {
   const { values, change, submit, errors } = props;
 
-  const onChange = evt => {};
-  const onSubmit = evt => {
-      evt.preventDefault();
+  const [userValues, setUserValues] = useState(initialUserValues);
+
+  const onChange = (evt) => {
+    const { name, value } = evt;
+    this.setState;
+  };
+  const onSubmit = (evt) => {
+    evt.preventDefault();
     //   submit();
   };
 
   return (
-
     <form className="login-container" onSubmit={onSubmit}>
       {/* ^^^^^^ come back to the onSubmit function ^^^^^^^^ */}
 
       <header id="loginHeader">
         <h2>Sign In</h2>
       </header>
-    <div id="loginForm" className="form">
-        <div className="row"><label>Username <input name="username" placeholder="Enter your username" type="text" /></label></div>
-        <div className="row"><label>Password <input name="password" placeholder="Enter your password" type="password" /></label></div>
-        <div className="row"><button id="loginBtn">Login</button></div>
-    </div>
-
+      <div id="loginForm" className="form">
+        <div className="row">
+          <label>
+            Username{" "}
+            <input
+              name="username"
+              placeholder="Enter your username"
+              type="text"
+              required
+              onChange
+            />
+          </label>
+        </div>
+        <div className="row">
+          <label>
+            Password{" "}
+            <input
+              name="password"
+              placeholder="Enter your password"
+              type="password"
+              required
+              onChange
+            />
+          </label>
+        </div>
+        <div className="row">
+          <button id="loginBtn">Sign In</button>
+        </div>
+      </div>
     </form>
   );
 }
