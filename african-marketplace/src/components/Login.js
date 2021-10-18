12ab95cb9ae02c231fb15c9fa4b8d9537a5bf8b7
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-10-18 19:40:57 -0400

    added validation

--- african-marketplace/src/components/Login.js
+++ african-marketplace/src/components/Login.js
@@ -1,20 +1,42 @@
 import React, { useState } from "react";
+import * as yup from "yup";
 import axios from "axios";
+
 
 const initialUserValues = {
   username: "",
   password: "",
 };
 
+const initialFormErrors = {
+  username: "",
+  password: "",
+};
+
 export default function Login() {
-  
-
+  // STATES
   const [userValues, setUserValues] = useState(initialUserValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
 
+  //EVENT HANDLERS
   const onChange = (evt) => {
     const { name, value } = evt.target;
-    setUserValues({...userValues, [name]: value});
+    validate(name, value);
+    setUserValues({ ...userValues, [name]: value });
   };
+
+  const validate = (name, value) => {
+    yup
+      .reach(validationSchema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: "" }))
+      .catch((err) => setFormErrors({ ...formErrors, [name]: err.errors[0] }));
+  };
+
+  const validationSchema = yup.object().shape({
+    username: yup.required("Username is required"),
+    password: yup.required("Password is required"),
+  });
 
   const onSubmit = (evt) => {
     evt.preventDefault();
@@ -37,10 +59,10 @@ export default function Login() {
               placeholder="Enter your username"
               type="text"
               value={userValues.username}
-              required
               onChange={onChange}
             />
           </label>
+          <div className="error">{formErrors.username}</div>
         </div>
         <div className="row">
           <label>
@@ -50,13 +72,15 @@ export default function Login() {
               placeholder="Enter your password"
               type="password"
               value={userValues.password}
-              required
               onChange={onChange}
             />
           </label>
+          <div className="error">{formErrors.password}</div>
         </div>
         <div className="row">
-          <button id="loginBtn">Sign In</button>
+          <button id="loginBtn" type="submit">
+            Sign In
+          </button>
         </div>
       </div>
     </form>
