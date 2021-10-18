db160e023d8f539317582df818d2287bf0183e31
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-10-18 18:30:39 -0400

    cleaned up onchange

--- african-marketplace/src/components/Login.js
+++ african-marketplace/src/components/Login.js
@@ -1,34 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
-// import yup
 
 const initialUserValues = {
   username: "",
   password: "",
 };
 
-const initialFormErrors = {
-  username: "",
-  password: "",
-};
-
-export default function Login(props) {
-  const { values, change, submit, errors } = props; //return to this placeholder
+export default function Login() {
+  
 
   const [userValues, setUserValues] = useState(initialUserValues);
-  const [formErrors, setFormErrors] = useState(initialFormErrors);
 
   const onChange = (evt) => {
     const { name, value } = evt.target;
-    validate(name, value);
     setUserValues({...userValues, [name]: value});
   };
-
-const validate = (name, value) => {
-    yup.reach(schema, name)
-    .validate(value).then(() => setFormErrors{...formErrors, [name]: ""})
-    .catch((err => setFormErrors({...formErrors, [name]: err.errors[0]})));
-};
 
   const onSubmit = (evt) => {
     evt.preventDefault();
