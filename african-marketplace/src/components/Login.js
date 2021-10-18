5f020e205e05793e2bc58735d3588d4a425b3dee
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-10-18 17:22:20 -0400

    login progress

--- african-marketplace/src/components/Login.js
+++ african-marketplace/src/components/Login.js
@@ -5,16 +5,24 @@ import axios from "axios";
 export default function Login(props) {
   const { values, change, submit, errors } = props;
 
+  const onChange = evt => {};
+  const onSubmit = evt => {
+      evt.preventDefault();
+    //   submit();
+  };
+
   return (
+
     <form className="login-container" onSubmit={onSubmit}>
       {/* ^^^^^^ come back to the onSubmit function ^^^^^^^^ */}
+
       <header id="loginHeader">
-        <h2>Login</h2>
+        <h2>Sign In</h2>
       </header>
     <div id="loginForm" className="form">
-        <input description="Username" placeholder="Enter your username" type="text" />
-        <input description="Password" placeholder="Enter your password" type="password" />
-        <button id="loginBtn">Login</button>
+        <div className="row"><label>Username <input name="username" placeholder="Enter your username" type="text" /></label></div>
+        <div className="row"><label>Password <input name="password" placeholder="Enter your password" type="password" /></label></div>
+        <div className="row"><button id="loginBtn">Login</button></div>
     </div>
 
     </form>
