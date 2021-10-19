8bf1640508e57c3bd729e325ee3dc9eb13f6b7ed
David Fletcher <davidfletcher@Davids-Air-2-11.lan>
2021-10-18 20:09:40 -0700

    finished signup form -- none yup

--- african-marketplace/src/components/SignUp.js
+++ african-marketplace/src/components/SignUp.js
@@ -1,74 +1,110 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
 
 const initialFormVals = {
-   name:'MyName',
-   email: "myEmail@aol.com",
-   username: "username here",
-   password: "password"
+   name:'',
+   email: "",
+   username: "",
+   password: "",
+   terms: false,
 }
 
 
-
 export default function SignUp(props){
-
-   useEffect(() => {
-      const postNewUser = (newUser) =>{
-         axios.post('', newUser)
-            .then(res =>{
-               console.log(res)
-            })
-            .catch()
-
-      }
-
-   }, [])
-
+   const [ user, setUser ] = useState([]);
+   const [ formVals, setFormVals ] = useState(initialFormVals);
+   
+   //submit new user
    const onSubmit = (evt) =>{
       evt.preventDefault()
-      props.submit()
+      submit(formVals)
    }
 
-   const onChange = (evt) =>{
-      const {name, value, } = evt.target
+   const submit = () =>{
+      const newUser ={
+         name: formVals.name .trim(),
+         email: formVals.email.trim(),
+         username: formVals.username.trim(),
+         password: formVals.password.trim(),
+         terms: !!formVals.terms
+      }
+      postNewUser(newUser)
+      console.log(newUser) //proof of working
+   }
 
+   // useEffect(() => {
+      const postNewUser = (newUser) =>{
+         axios.post('https://reqres.in/api/users', newUser)
+            .then(res =>{
+               setUser([res.data, ...user]) // will we actually need to spread this here?
+            })
+            .catch( err => console.error(err))
+            .finally(() => console.log('cleaning'))
+      }
+   //    postNewUser(user)
+
+   //  }, [])
+
+   // Changes here
+   const onChange = (evt) =>{
+      const {name, value, type, checked } = evt.target;
+      const valToUse = type === 'checkbox' ? checked : value;
+      update( name, valToUse );
+   }
+   const update = ( name, value ) =>{
+      //! validate( name, value );
+      setFormVals({...formVals, [name]: value})
    }
 
    return (
+      <div>
+
       <form id="signUp" onSubmit={onSubmit}>
          <label> Full Name (first last):
             <input
                type='text'
                name='name'
-               value={initialFormVals.name}
+               value={formVals.name}
                onChange={onChange}
-
-            />
+               
+               />
          </label>
          <label> Email:
             <input
                type='email'
                name='email'
-               value={initialFormVals.email}
+               value={formVals.email}
                onChange={onChange}
-            />
+               />
          </label>
          <label> Username:
             <input
                type='text'
                name='username'
-               value={initialFormVals.username}
+               value={formVals.username}
                onChange={onChange}
-            />
+               />
          </label>
          <label> Password:
             <input
                type='password'
                name='password'
-               value={initialFormVals.password}
+               value={formVals.password}
                onChange={onChange}
-            />
+               />
+         </label>
+         <label> Terms of service
+            <input
+               type='checkbox'
+               name='terms'
+               checked={formVals.terms}
+               onChange={onChange}
+               />   
          </label>
          <input type='submit' /* onClick={(() => <Route path='/welcome'/>)} *//>
       </form>
+     
+   </div>
    );
 }
