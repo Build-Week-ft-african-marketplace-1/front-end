c3c45b8221443c30aaf72a4dbc8438885b2f69e3
David Fletcher <davidfletcher@Davids-Air-2-21.lan>
2021-10-20 09:48:07 -0700

    schema should be ready to go

--- african-marketplace/src/validation/signupSchema.js
+++ african-marketplace/src/validation/signupSchema.js
@@ -3,8 +3,9 @@ import * as yup from "yup";
 const schema = yup.object().shape({
     username: yup.string().required("Username is required"),
     password: yup.string().required("Password is required"),
-    email: yup.string().required("Password is required"),
-    name: yup.string().required("Password is required"),
+    email: yup.string().required("Email is required"),
+    name: yup.string().required("Name is required"),
+    terms: yup.boolean().required("Must Accept the Terms of Service"),
 
   });
 
