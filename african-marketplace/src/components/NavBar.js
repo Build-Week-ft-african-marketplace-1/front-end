fd58a160dee8613261ddb5e590c8a4fc2c6c4fca
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-10-20 13:27:16 -0400

    added logo to navbar

--- african-marketplace/src/components/NavBar.js
+++ african-marketplace/src/components/NavBar.js
@@ -4,39 +4,46 @@ import { connect } from "react-redux";
 import styled from "styled-components";
 
 const StyledNav = styled.nav`
-    background-color: white;
-    padding: 2em;
-    opacity: 70%;
-`
+  background-color: white;
+  padding: 2em;
+  opacity: 70%;
+`;
 
 const StyledLink = styled(Link)`
-    padding-left: 2em;
-    padding-right: 2em;
-    text-decoration: none;
-    color: black;
-    font-size: 1.5em;
-    &:hover {
-        color: teal;
-        text-decoration: underline;
-    }
-`
+  padding-left: 2em;
+  padding-right: 2em;
+  text-decoration: none;
+  color: black;
+  font-size: 1.5em;
+  &:hover {
+    color: teal;
+    text-decoration: underline;
+  }
+`;
+const StyledLogo = styled.img`
+  width: 100px;
+  position: absolute;
+  z-index: 9;
+  left: 2rem;
+  top: 1.5rem;
+`;
 
 const NavBar = () => {
-
-return (
+  return (
     <div className="nav-header">
-        <header>
-            <StyledNav>
-                <StyledLink to="/">Login</StyledLink>
-                <StyledLink to="/signup">Sign Up</StyledLink>
-                <StyledLink to="/homepage">Home</StyledLink>
-                <StyledLink to="/addproduct">Add Product</StyledLink>
-                <StyledLink to="/productlist">Product List</StyledLink>
-                <StyledLink to ="/logout">Logout</StyledLink>
-            </StyledNav>
-        </header>
+      <header>
+        <StyledLogo src="logo.png" />
+        <StyledNav>
+          <StyledLink to="/">Login</StyledLink>
+          <StyledLink to="/signup">Sign Up</StyledLink>
+          <StyledLink to="/homepage">Home</StyledLink>
+          <StyledLink to="/addproduct">Add Product</StyledLink>
+          <StyledLink to="/productlist">Product List</StyledLink>
+          <StyledLink to="/logout">Logout</StyledLink>
+        </StyledNav>
+      </header>
     </div>
-    );
+  );
 };
 
 export default NavBar;
