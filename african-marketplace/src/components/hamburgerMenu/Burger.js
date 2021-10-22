a952b322b18eada2eff0b848fac9923c4395d7e6
David Fletcher <davidfletcher@Davids-Air-2.lan>
2021-10-22 14:27:56 -0700

    modified Burger to take prop-types

--- african-marketplace/src/components/hamburgerMenu/Burger.js
+++ african-marketplace/src/components/hamburgerMenu/Burger.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { StyledBurger } from '../styles/OurStyles';
 
-const Burger = () => {
+//These are new to me
+import { bool, func } from 'prop-types';
+
+const Burger = (props) => {
+  const { setOpen, open } = props;
   return (
     <StyledBurger>
       <div />
